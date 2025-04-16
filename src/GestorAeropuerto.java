f62993fc655f26b3dcb59fd9ca7dc732b6d53eeb
R0ber-t <robert.val11@yahoo.es>
2025-04-16 17:33:55 +0200

    GestorAeropuerto

--- src/GestorAeropuerto.java
+++ src/GestorAeropuerto.java
@@ -1,17 +1,40 @@
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 
 public class GestorAeropuerto extends Usuario {
     private Set<String> preferenciasNotificaciones;
+    private int diasAnticipados = 30;
 	
 	public GestorAeropuerto(String nombre, String contraseña) {
 		super(nombre, contraseña);
         this.preferenciasNotificaciones = new HashSet<>(); // Set para preferencias de notificaciones
-	}    
+	}   
+    
+    public int getDiasAnticipacionMinima() {
+        return diasAnticipados;
+    }
+
+    public boolean setDiasAnticipacionMinima(int nuevosDias) {
+        if (nuevosDias >= 1 && nuevosDias <= 365) {
+            this.diasAnticipados = nuevosDias;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     public void agregarPreferenciaNotificacion(String preferencia) {
         preferenciasNotificaciones.add(preferencia);
+    }
+
+    public boolean puedeCrearVuelo(LocalDateTime fechaHoraVuelo){
+        LocalDateTime hoy = LocalDateTime.now();
+        LocalDateTime fechaMinima = hoy.plusDays(diasAnticipados);
+
+        return fechaHoraVuelo.isAfter(fechaMinima);
     }
 
 
