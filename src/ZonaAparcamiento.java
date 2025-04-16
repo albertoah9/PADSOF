09b9e04efb07b3ac6d06f041ab60dda95107c005
R0ber-t <robert.val11@yahoo.es>
2025-04-16 17:39:33 +0200

    ZonaAparcamiento

--- src/ZonaAparcamiento.java
+++ src/ZonaAparcamiento.java
@@ -1,25 +1,20 @@
 
 public class ZonaAparcamiento extends ElementoAeropuerto {
-    public enum TipoZonaAparcamiento{
-        PASAJEROS, MERCANCIAS
-    }
+    
 
     /* Atributos */
     private double ancho;
     private double largo;
     private int numPlazas;
     private double costePorHora;
-    private TipoZonaAparcamiento tipoZonaAparcamiento;
 
     private int plazasOcupadas = 0;
 
-    public ZonaAparcamiento(double ancho, double largo, int numPlazas,
-            ZonaAparcamiento.TipoZonaAparcamiento tipoZonaAparcamiento) {
+    public ZonaAparcamiento(double ancho, double largo, int numPlazas) {
         super();
         this.ancho = ancho;
         this.largo = largo;
         this.numPlazas = numPlazas;
-        this.tipoZonaAparcamiento = tipoZonaAparcamiento;
     }
     
     public double getCostePorHora() {
@@ -54,13 +49,6 @@ public class ZonaAparcamiento extends ElementoAeropuerto {
         this.numPlazas = numPlazas;
     }
 
-    public TipoZonaAparcamiento getTipoZonaAparcamiento() {
-        return tipoZonaAparcamiento;
-    }
-
-    public void setTipoZonaAparcamiento(TipoZonaAparcamiento tipoZonaAparcamiento) {
-        this.tipoZonaAparcamiento = tipoZonaAparcamiento;
-    }
 
     public double calcularArea() {
         return ancho * largo;
@@ -89,7 +77,6 @@ public class ZonaAparcamiento extends ElementoAeropuerto {
     @Override
     public String toString() {
         return "Zona de Aparcamiento ID: " + getId() + "\n" +
-            "Tipo: " + tipoZonaAparcamiento + "\n" +
             "Tamaño: " + ancho + "m x " + largo + "m (" + calcularArea() + "m²)\n" +
             "Número de plazas: " + numPlazas + "\n" +
             "Coste por hora: " + costePorHora + "€/hora\n" +
